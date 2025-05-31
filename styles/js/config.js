0e9927d7d91a31d1e34b8e3a4265041ad70746da
KamileonBuns <annabunnybun@gmail.com>
2025-06-01 00:20:26 +0200

    commit

--- styles/js/config.js
+++ styles/js/config.js
@@ -44,8 +44,8 @@ charadex.sheet = {
 
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
     statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
-    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Dog', 'Cat', 'Bunny'],
+    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Sparse', 'Extinct', 'Old world'],
+    species: ['All', 'Wakatrick', 'Virus', 'Sentinel'],
     itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
     traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
 
