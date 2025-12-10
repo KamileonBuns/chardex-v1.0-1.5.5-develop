7fa6249c2831e5098040e40805ab8925af24c0ae
KamileonBuns <annabunnybun@gmail.com>
2025-12-10 17:40:58 +0100

    commit

--- styles/js/config.js
+++ styles/js/config.js
@@ -44,10 +44,10 @@ charadex.sheet = {
 
     designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
     statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
-    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Mythical', 'Ethereal', 'Mutation', 'Unseen', 'Affliction', 'Veil-Touched','sparse', 'error', 'virus'],
+    rarity: ['All', 'sparse', 'error', 'virus'],
     species: ['All', 'Wakatrick', 'Moka', 'Zoa', 'Clockmaw', 'ArcNyx', 'Greetabye', ],
     itemTypes: ['All', 'Currency', 'MYO Slot', 'Trinket', 'Trait upgrade', 'Misc', 'ingredient', 'event'],
-    traitTypes: ['All', 'Clockmaw Traits', 'ArcNyx Traits', 'Greetabye Traits','Universal','[WK]Texture', '[WK]Required', '[WK]untraited','[WK]Upgrade', '[WK]Enhancement', '[WK] Error', '[WK]Virus related']
+    traitTypes: ['All', 'Texture', 'Required', 'untraited','Upgrade', 'Enhancement', 'Error', 'Virus related']
 
   }
 
